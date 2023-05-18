9bea319008ca2944c7fdd315d09ff9a51cfc5ed1
Lot Kyçyku <lotkyqyku@gmail.com>
2023-05-18 11:29:20 +0200

    Add Restaurant model, migration, factory, and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         \App\Models\User::factory(15)->create();
+        \App\Models\Restaurant::factory(30)->create();
+
 
         \App\Models\User::factory()->create([
             'name' => 'Loti',
