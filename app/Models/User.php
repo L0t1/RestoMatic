e6a033f2a9d410348559600712fd0bb92e89223c
Lot Kyçyku <lotkyqyku@gmail.com>
2023-05-18 13:14:14 +0200

    Added normal/owner role of user model and the relationship with reservation and restaurant

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,10 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+     // Define the role constants
+     public const ROLE_NORMAL = 'normal';
+     public const ROLE_OWNER = 'owner';
+
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +25,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -41,4 +46,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // Relationship with Reservation model (User has many Reservations)
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    // Relationship with Restaurant model (User can own a Restaurant)
+    public function restaurant()
+    {
+        return $this->hasOne(Restaurant::class, 'owner_id');
+    }
+
+    // Check if the user has the 'normal' role
+    public function isNormalUser()
+    {
+        return $this->role === self::ROLE_NORMAL;
+    }
+
+    // Check if the user has the 'owner' role
+    public function isOwnerUser()
+    {
+        return $this->role === self::ROLE_OWNER;
+    }
 }
